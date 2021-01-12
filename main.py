9a06c03c1427cd64dda5322f9834a74f16eeab87
Bjarturl <bjarturluk@gmail.com>
2021-01-12 19:46:21 +0000

    Added cors config

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import flask
 from flask import request
 from helpers import get_random_word_from_file
-
+from flask_cors import CORS, cross_origin
 app = flask.Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 @app.route('/random_ord', methods=['GET'])
+@cross_origin()
 def home():
     word = get_random_word_from_file()
     return {
